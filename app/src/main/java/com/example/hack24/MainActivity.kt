e0767348b3ca441ba9813b916dc5405c18da9cfc
triplesh0t <ffrench2004@gmail.com>
2024-04-28 03:49:50 -0700

    prog

--- app/src/main/java/com/example/hack24/MainActivity.kt
+++ app/src/main/java/com/example/hack24/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.hack24
 
 import android.os.Bundle
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,10 +11,13 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.core.content.ContextCompat
+import androidx.core.app.ActivityCompat
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.hack24.ui.theme.Hack24Theme
 
 class MainActivity : ComponentActivity() {
+    private val CALENDAR_PERMISSION_CODE = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,6 +26,19 @@ class MainActivity : ComponentActivity() {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Greeting("Android")
                 }
+            }
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_CALENDAR), CALENDAR_PERMISSION_CODE)
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_CALENDAR), CALENDAR_PERMISSION_CODE)
+        }
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) || ((ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_CALENDAR) != PackageManager.PERMISSION_GRANTED))) {
+            setContent {
+                Text(
+                    text = "HELLO YOU NEED TO GIVE CALENDAR PERMISSIONS TO USE THIS APP!!!"
+                )
             }
         }
     }
